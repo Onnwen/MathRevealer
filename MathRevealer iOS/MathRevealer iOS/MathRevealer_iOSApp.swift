d158ef6af90f02c7c04f0066cc1b2bba1e9af19b
Onnwen <onnwen.cassitto@icloud.com>
2022-10-23 13:41:58 +0200

    Inizializzato progetto iOS

---
+++ MathRevealer iOS/MathRevealer iOS/MathRevealer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathRevealer_iOSApp.swift
+//  MathRevealer iOS
+//
+//  Created by Onnwen Cassitto on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MathRevealer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
